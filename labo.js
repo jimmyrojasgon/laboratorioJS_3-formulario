43f4a026717f8ee38351b93bb32d429d3c4d83ee
jimmyrojasgon <77075586@certus.edu.pe>
2024-06-03 22:51:48 -0500

    correciones

--- labo.js
+++ labo.js
@@ -5,21 +5,33 @@ e.preventDefault();
 
 const usuario = document.getElementById('usuario').value;
 
+const usuarioError = document.getElementById('usuarioError');
+const passError = document.getElementById('repetirContraseñaError');
+
 let esValido = true;
 
+const usuarioRegex = ^[a-zA-Z0-9_]{4,16}$/
 if(usuario === ''){
-document.getElementById('usuarioError').textContent = 'el nombre de usuario es obligatorio';
+usuarioError.textContent = 'el nombre de usuario es obligatorio';
 esValido = false;
+}else if(!usuarioRegex.test(usuario)){
+    usuarioError.textContent = 'el nombre de usuario debe tener minimo 5 y 16 digitos y sol puede contener numero y quien bajo';
+    
+    esValido = false
 }
+
+
 
 if(usuario === ''){
 document.getElementById('usuarioError').textContent = 'el nombre de usuario es obligatorio';
 esValido = false;
 }
+
+if(pass1 === pass2)
 
 if (esValido == true) {
 alert('formulario valido')
 }
 
 console.log('hola');
-}
+}    
